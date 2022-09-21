cf52e4f58a74af13e2ea911df5db5e8818ddf357
Daniel Peixoto <danielgomes0@gmail.com>
2022-09-21 16:42:53 +0100

    commit adc function

---
+++ 9_ADC_Continuous_Conversion/Inc/uart.h
@@ -0,0 +1,17 @@
+/*
+ * uart.h
+ *
+ *  Created on: 17/08/2022
+ *      Author: daniel
+ */
+
+#ifndef UART_H_
+#define UART_H_
+#include "stm32l4xx.h"
+
+void uart2_tx_init(void);
+char uart2_read(USART_TypeDef *USARTx);
+static void uart2_write(USART_TypeDef *USARTx,uint8_t ch);
+void uart2_rxtx_init(void);
+
+#endif /* UART_H_ */
