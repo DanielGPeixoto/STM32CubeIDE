a28016e553becbd744623adbed291db1cdfb4367
Paulo <cruzsjpaulo@gmail.com>
2022-09-26 19:17:45 +0100

    Commit PWM - Paulo

---
+++ 12_PWMv2/Debug/Src/pwm.d
@@ -0,0 +1,17 @@
+Src/pwm.o: ../Src/pwm.c \
+ /home/cruz/Documents/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ /home/cruz/Documents/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h \
+ /home/cruz/Documents/chip_headers/CMSIS/Include/core_cm4.h \
+ /home/cruz/Documents/chip_headers/CMSIS/Include/cmsis_version.h \
+ /home/cruz/Documents/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ /home/cruz/Documents/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ /home/cruz/Documents/chip_headers/CMSIS/Include/mpu_armv7.h \
+ /home/cruz/Documents/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h
+/home/cruz/Documents/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+/home/cruz/Documents/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h:
+/home/cruz/Documents/chip_headers/CMSIS/Include/core_cm4.h:
+/home/cruz/Documents/chip_headers/CMSIS/Include/cmsis_version.h:
+/home/cruz/Documents/chip_headers/CMSIS/Include/cmsis_compiler.h:
+/home/cruz/Documents/chip_headers/CMSIS/Include/cmsis_gcc.h:
+/home/cruz/Documents/chip_headers/CMSIS/Include/mpu_armv7.h:
+/home/cruz/Documents/chip_headers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
